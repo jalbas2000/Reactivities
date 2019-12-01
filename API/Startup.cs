0ef7a1ea98c65563e509a0f09a46577081189664
jalbas2000 <jalbas2000@yahoo.com>
2019-12-01 12:24:55 +0400

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,12 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -42,7 +48,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
