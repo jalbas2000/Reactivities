ebb3c0ff9f2f6b754b085af9f3a4f12570b8f780
jalbas2000 <jalbas2000@yahoo.com>
2020-02-05 22:49:51 +0400

    End of Section 20

--- client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
@@ -6,6 +6,11 @@ interface IProps {
     attendees: IAttendee[]
 }
 
+const styles = {
+    borderColor: 'orange',
+    borderWidth: 2
+}
+
 const ActivityListItemAttendees: React.FC<IProps> = ({attendees}) => {
     return (
         <List horizontal>
@@ -13,7 +18,11 @@ const ActivityListItemAttendees: React.FC<IProps> = ({attendees}) => {
                 <List.Item key={attendee.username}>
                     <Popup 
                         header={attendee.displayName}
-                        trigger={<Image size='mini' circular src={attendee.image || '/assets/user.png'} />}
+                        trigger={
+                            <Image size='mini' circular bordered 
+                                style={attendee.following ? styles : null}
+                                src={attendee.image || '/assets/user.png'} />
+                        }
                     />
                 </List.Item>
             ))};
